1d5ab57e245945fe170d5f7f8e126fb065e81510
Zuke27 <wstaylor27@gmail.com>
2021-10-05 22:51:01 -0400

    Updated index.js and markdown

--- index.js
+++ index.js
@@ -1,57 +1,126 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
 
 // TODO: Create an array of questions for user input
 const questions = [
     {
     type: 'input',
     name: 'title',
-    message: 'What is your project title?',
+    message: 'What is your repository title?',
+    validate: titleInput => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log('Please enter a repository title.')
+      }
+    }
   },
   {
     type: 'input',
     name: 'description',
     message: 'What is your description of this project?',
+    validate: descriptionInput => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log('Please describe the project.')
+      }
+    }
   },
   {
     type: 'input',
     name: 'install',
     message: 'What are the installation instructions?',
+    validate: installInput => {
+      if (installInput) {
+        return true;
+      } else {
+        console.log('Please enter installation instructions.')
+      }
+    }
   },
   {
     type: 'input',
     name: 'usage',
     message: 'What is the usage information?',
+    validate: usageInput => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log('Please state usage information.')
+      }
+    }
   },
   {
     type: 'input',
     name: 'guidelines',
     message: 'Enter the contribution guidelines',
+    validate: guidelinesInput => {
+      if (guidelinesInput) {
+        return true;
+      } else {
+        console.log('Please enter contribution guidelines')
+      }
+    }
   },
   {
     type: 'input',
     name: 'tests',
-    message: 'Enter your your tests',
+    message: 'Enter your how tested the project',
+    validate: testsInput => {
+      if (testsInput) {
+        return true;
+      } else {
+        console.log('Please enter how you tested your project')
+      }
+    }
   },
   {
-    type: 'input',
+    type: 'checkbox',
     name: 'license',
     message: 'Choose a license for your application',
+    choices: ['MIT', 'GNU', 'ISC', 'Apache', 'None'],
+    validate: licenseInput => {
+      if (licenseInput) {
+        return true;
+      } else {
+        console.log('Please pick a license')
+      }
+    }
   },
   {
     type: 'input',
-    name: 'Github',
+    name: 'github',
     message: 'Enter your GitHub username',
+    validate: githubInput => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log('Please enter your GitHub username.')
+      }
+    }
   }
 
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeToFile(fileName, data, (err) => {
+    if (err)
+    throw err;
+    console.log('Success!')
+  })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generateMarkdown(userInput));
+})}
 
 // Function call to initialize app
 init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,42 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+  ## Licensing:
+  [![license](https://img.shields.io/badge/license-${data.license}-blue)](https://shields.io)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.install}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.license}
+  ## Contribution:
+  ${data.guidelines}
+  ## Testing:
+  ${data.tests}
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})`;
+};
+
+module.exports = generateMarkdown;
